980268ac13f3394e674094bbf3dbc2f9031b0653
k5va <k5va@yandex.ru>
2023-05-03 19:51:26 +0500

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# spinner
+
+React button component with animation
+
+Thanks to Adey Coder https://www.youtube.com/@AdeyCoder for the inspiration!
---
+++ src/components/app/App.tsx
@@ -0,0 +1,7 @@
+import styles from './App.module.scss';
+
+function App() {
+  return <div className={styles.container}>App...</div>;
+}
+
+export default App;
